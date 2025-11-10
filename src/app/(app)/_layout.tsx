23b6fa3cfaa07ad6b20e851a1b2581f2ebe60cf8
VishalKumar Mishra <141928823+vishalpandit20@users.noreply.github.com>
2025-11-10 10:56:28 +0530

    Implement authentication flow with protected routes and add sign-in and sign-up screens

--- src/app/(app)/_layout.tsx
+++ src/app/(app)/_layout.tsx
@@ -1,8 +1,19 @@
+import { useAuth } from "@clerk/clerk-expo"
 import { Stack } from "expo-router"
 
 function Layout () {
+    const {isLoaded, isSignedIn, userId, sessionId, getToken} = useAuth();
+
+    console.log("isSignedIn >>>>", isSignedIn)
     return <Stack>
-        <Stack.Screen name="(tabs)" options={{headerShown: false}}/> 
+        <Stack.Protected guard={isSignedIn}>
+            <Stack.Screen name="(tabs)" options={{headerShown: false}}/>
+        </Stack.Protected>
+
+        <Stack.Protected guard={!isSignedIn}>
+            <Stack.Screen name="sign-in" options={{headerShown: false}}/>
+            <Stack.Screen name="sign-up" options={{headerShown: false}}/>
+        </Stack.Protected>
     </Stack>
 }
 export default Layout
